df94dad1c933eee66f853402b3096ade8c335a18
Keira <94308338+KeiraYeet@users.noreply.github.com>
2025-11-17 10:25:14 +0700

    Readability changes

--- src/main/java/hospital/controller/EmployeeController.java
+++ src/main/java/hospital/controller/EmployeeController.java
@@ -2,8 +2,11 @@ package hospital.controller;
 
 import hospital.database.EmployeeRepository;
 import hospital.staffClasses.Employee;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.mongodb.core.MongoTemplate; // <-- 1. IMPORT THIS
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,30 +15,31 @@ import java.util.List;
 
 @RestController 
 @RequestMapping("/api/employees") 
-// This allows both ports, which is correct
+// This allows both ports
 @CrossOrigin(origins = {"http://localhost:5173", "http://localhost:5174"})
 public class EmployeeController {
 
     @Autowired 
     private EmployeeRepository employeeRepository;
 
-    // 2. ADD THE MONGO TEMPLATE
     @Autowired
     private MongoTemplate mongoTemplate;
 
-    // 3. THIS IS THE STABLE, WORKING METHOD
     @GetMapping
     public List<Object> getAllEmployees() {
         
-        // --- 4. THIS IS THE DEBUG PRINTOUT ---
         System.out.println("--- DEBUG: getAllEmployees() method was called! ---");
         
-        // This is the simple workaround that we know works.
-        return mongoTemplate.findAll(Object.class, "employee");
+        // Create a new query
+        Query query = new Query();
+
+        // Tell the query to sort by your "id" field, Ascending
+        query.with(Sort.by(Sort.Direction.ASC, "id"));
+
+        return mongoTemplate.find(query, Object.class, "employee");
     }
-    
-    // Your "Add Employee" method (this will still have the "abstract class" error,
-    // but at least your "Get" will work)
+
+    // Pending
     @PostMapping
     public ResponseEntity<?> createEmployee(@RequestBody Employee employee) {
         try {
